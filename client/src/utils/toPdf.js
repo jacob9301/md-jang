8cff518507fd10c95fd9487f39e1d113902bbbfd
jacob9301 <jacob.luc.bushby@gmail.com>
2023-07-05 17:13:15 +0100

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/utils/toPdf.js

--- client/src/utils/toPdf.js
+++ client/src/utils/toPdf.js
@@ -1,4 +1,5 @@
 import html2pdf from 'html2pdf.js';
+import jsPDF from 'jspdf';
 
 import store from '../store';
 
@@ -10,14 +11,21 @@ const generatePDF = () => {
     const fileName = encodeURIComponent(state.currentNote.title + '.pdf');
 
     const opt = {
-        margin:       1,
+        margin:       [0,0],
         filename:     fileName,
         image:        { type: 'jpeg', quality: 0.98 },
-        html2canvas:  { scale: 2 },
+        html2canvas:  { dpi: 192, letterRendering: true },
         jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
     };
 
-    html2pdf().from(elementsToExport).set(opt).save();
+    const doc = new jsPDF(opt.jsPDF);
+    const pageSize = jsPDF.getPageSize(opt.jsPDF);
+
+    const pageImage = await html2pdf().from(page).set(opt).outputImg();
+
+    doc.addImage(pageImage.src, 'jpeg', opt.margin[0], opt.margin[1], pageSize.width, pageSize.height);
+
+    doc.save(fileName);
 };
 
 export default generatePDF;
